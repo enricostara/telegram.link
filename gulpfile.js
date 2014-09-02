2c6a94d8c1135c23a6318dc87faab3d423957695
Enrico Stara <enrico.stara@gmail.com>
2014-09-03 00:27:19 +0200

    Gulp build file added

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var docco = require('gulp-docco');
+
+gulp.task('src', function () {
+    return gulp.src('lib/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(docco({'layout': 'linear'}))
+        .pipe(gulp.dest('./docs/api'));
+});
+
+gulp.task('test', function () {
+    return gulp.src('./test/**/*.js')
+        .pipe(mocha({reporter: 'tap', timeout: '10s'}));
+});
+
+gulp.task('integration', function () {
+    return gulp.src('./integration/**/*.js')
+        .pipe(mocha({reporter: 'tap', timeout: '10s'}));
+});
+
+gulp.task('default', ['src', 'test']);
