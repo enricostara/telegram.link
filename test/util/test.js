64e8bcc217299f0c4fe69ec62a80493cee3050f2
Enrico Stara <enrico.stara@gmail.com>
2014-10-02 20:43:05 +0200

    Add test for ‘retryUntilItsDone’ function

--- test/util/test.js
+++ test/util/test.js
@@ -1,57 +1,44 @@
 require('should');
-var Vector = require('../../index').type_language.Vector;
-var AbstractObject = require('../../index').type_language.AbstractObject;
+var util = require('../../index').util;
 
-describe('Vector', function () {
+describe('util', function () {
 
-    describe('#init()', function () {
-        it('should return an instance', function (done) {
-            var list = new Vector();
-            list.should.be.ok;
-            list.should.be.an.instanceof(Vector);
-            list.should.be.an.instanceof(AbstractObject);
-            list.should.have.properties({id: '15c4b51c', type: 'Int'});
-            list.isReadonly().should.be.false;
-
-            var list = new Vector({type: 'long', buffer: new Buffer('15C4B51C01000000216BE86C022BB4C3', 'hex')});
-            list.should.have.properties({id: '15c4b51c', type: 'Long'});
-            list.isReadonly().should.be.true;
-
-            var list = new Vector({type: 'long', list: [1,2,3]});
-            list.should.have.properties({id: '15c4b51c', type: 'Long'});
-            list.isReadonly().should.be.false;
-            list.getList().should.be.eql([1,2,3]);
-
-            done();
+    describe('#retryUntilItsDone()', function () {
+        it('should retry until limit is reached ', function (done) {
+            var i = 0;
+            function task(callback) {
+                setTimeout(function () {
+                    i++;
+                    callback('err')
+                }, 10);
+            }
+            util.retryUntilItsDone(task, 5, function (ex) {
+                ex.should.be.eql('err');
+                i.should.be.equal(5);
+                done();
+            })
         })
     });
-
-    describe('#deserialize()', function () {
-        it('should de-serialize the list', function (done) {
-            var list = new Vector({type: 'long', buffer: new Buffer('15C4B51C01000000216BE86C022BB4C3', 'hex')});
-            list.deserialize().should.be.ok;
-            list.getList().length.should.be.equal(1);
-            list.getList().pop().should.be.equal('0xc3b42b026ce86b21');
-            done();
+    describe('#retryUntillItsDone()', function () {
+        it('should retry until done ', function (done) {
+            var i = 0;
+            function task(callback) {
+                setTimeout(function () {
+                    i++;
+                    if(i===3) {
+                        callback(null, i, i);
+                    } else {
+                        callback('err')
+                    }
+                }, 10);
+            }
+            util.retryUntilItsDone(task, null, function (ex, arg1, arg2) {
+                (!ex).should.be.true;
+                arg1.should.be.eql(3);
+                arg2.should.be.eql(3);
+                i.should.be.equal(3);
+                done();
+            })
         })
     });
-
-    describe('#deserialize()', function () {
-        it('should not de-serialize the list cause type id mismatch', function (done) {
-            var list = new Vector({type: 'long', buffer: new Buffer('25C4B51C01000000216BE86C022BB4C3', 'hex')});
-            list.deserialize().should.not.be.ok;
-            done();
-        })
-    });
-
-    describe('#serialize()', function () {
-        it('should serialize the list', function (done) {
-            var list = new Vector({type: 'long', list: ['0xc3b42b026ce86b21']});
-            var buffer =  list.serialize();
-            buffer.should.be.ok;
-            buffer.toString('hex').should.be.equal('15c4b51c01000000216be86c022bb4c3');
-            done();
-        })
-    });
-
 });
