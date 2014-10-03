66c85a9653aaf7f816944229bb396d9cb478e659
Enrico Stara <enrico.stara@gmail.com>
2014-10-03 15:56:34 +0200

    Improve ‘retryUntilItsDone’ utility function

--- test/util/test.js
+++ test/util/test.js
@@ -6,39 +6,40 @@ describe('util', function () {
     describe('#retryUntilItsDone()', function () {
         it('should retry until limit is reached ', function (done) {
             var i = 0;
-            function task(callback) {
+            function task(callback, input) {
                 setTimeout(function () {
                     i++;
-                    callback('err')
+                    callback('err+' + input)
                 }, 10);
             }
             util.retryUntilItsDone(task, 5, function (ex) {
-                ex.should.be.eql('err');
+                ex.should.be.eql('err+foo');
                 i.should.be.equal(5);
                 done();
-            })
+            }, 'foo')
         })
     });
     describe('#retryUntillItsDone()', function () {
         it('should retry until done ', function (done) {
             var i = 0;
-            function task(callback) {
+            function task(callback, input) {
                 setTimeout(function () {
                     i++;
                     if(i===3) {
-                        callback(null, i, i);
+                        callback(null, i, i, input);
                     } else {
                         callback('err')
                     }
                 }, 10);
             }
-            util.retryUntilItsDone(task, null, function (ex, arg1, arg2) {
+            util.retryUntilItsDone(task, null, function (ex, arg1, arg2, input) {
                 (!ex).should.be.true;
                 arg1.should.be.eql(3);
                 arg2.should.be.eql(3);
+                input.should.be.eql('foo');
                 i.should.be.equal(3);
                 done();
-            })
+            }, 'foo');
         })
     });
 });
