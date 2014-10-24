4be1600626b49664ab652382ed2534a68ed940dd
Enrico Stara <enrico.stara@gmail.com>
2014-10-24 14:58:48 +0200

    Improve prime factorization function

--- test/crypto/pq_finder.test.js
+++ test/crypto/pq_finder.test.js
@@ -7,7 +7,7 @@ describe('PQFinder', function () {
         it('should create a PQFinder instance giving a number', function (done) {
             var pqFinder = new PQFinder(100);
             pqFinder.should.be.ok;
-            (pqFinder._pqNumber == 100).should.be.true;
+            (pqFinder._pqNumber.equalsNum(100)).should.be.true;
             done();
         })
     });
@@ -16,7 +16,7 @@ describe('PQFinder', function () {
         it('should create a PQFinder instance giving a String', function (done) {
             var pqFinder = new PQFinder('123456789');
             pqFinder.should.be.ok;
-            (pqFinder._pqNumber == 123456789).should.be.true;
+            (pqFinder._pqNumber.equalsNum(123456789)).should.be.true;
             done();
         })
     });
@@ -25,7 +25,7 @@ describe('PQFinder', function () {
         it('should create a PQFinder instance giving a Buffer', function (done) {
             var pqFinder = new PQFinder(new Buffer('075BCD15', 'hex'));
             pqFinder.should.be.ok;
-            (pqFinder._pqNumber == 123456789).should.be.true;
+            (pqFinder._pqNumber.equalsNum(123456789)).should.be.true;
             done();
         })
     });
@@ -34,12 +34,12 @@ describe('PQFinder', function () {
         it('should find P and Q returning as [p, q]', function (done) {
 
             var pqFinder = new PQFinder(new Buffer('17ED48941A08F981', 'hex'));
-            console.log(pqFinder._pqNumber.toString(10));
-            console.log(pqFinder.findPQ()[0].toString(16));
-            console.log(pqFinder.findPQ()[1].toString(16));
+            console.log(pqFinder._pqNumber.toStr(10));
+            console.log(pqFinder.findPQ()[0].toStr(16));
+            console.log(pqFinder.findPQ()[1].toStr(16));
             console.log(pqFinder.getPQAsBuffer());
-            (pqFinder.findPQ()[0] == 1229739323).should.be.true;
-            (pqFinder.findPQ()[1] == 1402015859).should.be.true;
+            (pqFinder.findPQ()[0].equalsNum(1229739323)).should.be.true;
+            (pqFinder.findPQ()[1].equalsNum(1402015859)).should.be.true;
 
             done();
         })
