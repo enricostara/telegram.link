93fceedbb37793894bd29e58d54cba148a3ffb4a
Enrico Stara <enrico.stara@gmail.com>
2014-11-15 00:20:26 +0100

    Add client DH data management in `authorization`
    - create client DH inner data
    
    - encrypt client DH inner data
    
    - update some log messages

--- telegram.link.js
+++ telegram.link.js
@@ -66,9 +66,11 @@ TelegramLink.prototype.authorization = function (callback) {
                 findPAndQ,
                 findPublicKey,
                 createPQInnerData,
-                encryptDataWithRSA,
+                encryptPQInnerDataWithRSA,
                 requestDHParams,
-                decryptDHParams
+                decryptDHParams,
+                createClientDHInnerData,
+                encryptClientDHInnerDataWithAES
             ], callback);
         });
 
@@ -139,14 +141,14 @@ TelegramLink.prototype.authorization = function (callback) {
         return obj;
     }
 
-    // Encrypt the pq_inner_data
-    function encryptDataWithRSA(obj) {
+    // Encrypt the pq_inner_data with RSA
+    function encryptPQInnerDataWithRSA(obj) {
         // Create the data with hash to be encrypt
         var hash = crypto.createSHA1Hash(obj.pqInnerData);
         var dataWithHash = Buffer.concat([hash, obj.pqInnerData]);
         if (logger.isDebugEnabled()) {
-            logger.debug('Data to be encrypt contain: hash(%s), pqInnerData(%s), total length %s',
-                hash.toString('hex'), obj.pqInnerData.toString('hex'), dataWithHash.length);
+            logger.debug('Data to be encrypted contains: hash(%s), pqInnerData(%s), total length %s',
+                hash.length, obj.pqInnerData.length, dataWithHash.length);
         }
         // Encrypt data with RSA
         obj.encryptedData = crypto.rsaEncrypt({message: dataWithHash, key: obj.publicKey});
@@ -192,27 +194,33 @@ TelegramLink.prototype.authorization = function (callback) {
     function decryptDHParams(obj, duration) {
         var newNonce = AbstractObject.stringValue2Buffer(obj.newNonce, 32);
         var serverNonce = AbstractObject.stringValue2Buffer(obj.resPQ.server_nonce, 16);
-        if (logger.isDebugEnabled()) logger.debug('newNonce = %s, serverNonce = %s', newNonce.toString('hex'), serverNonce.toString('hex'));
+        if (logger.isDebugEnabled()) {
+            logger.debug('newNonce = %s, serverNonce = %s', newNonce.toString('hex'), serverNonce.toString('hex'));
+        }
         var hashNS = crypto.createSHA1Hash([newNonce, serverNonce]);
-        if (logger.isDebugEnabled()) logger.debug('hashNS = %s', hashNS.toString('hex'));
         var hashSN = crypto.createSHA1Hash([serverNonce, newNonce]);
-        if (logger.isDebugEnabled()) logger.debug('hashSN = %s', hashSN.toString('hex'));
         var hashNN = crypto.createSHA1Hash([newNonce, newNonce]);
-        if (logger.isDebugEnabled()) logger.debug('hashNN = %s', hashNN.toString('hex'));
+        if (logger.isDebugEnabled()) {
+            logger.debug('hashNS = %s, hashSN = %s, hashNN = %s', hashNS.toString('hex'), hashSN.toString('hex'), hashNN.toString('hex'));
+        }
+        // Create the AES key
         var aesKey = Buffer.concat([hashNS, hashSN.slice(0, 12)]);
-        if (logger.isDebugEnabled()) logger.debug('aesKey = %s', aesKey.toString('hex'));
         var aesIv = Buffer.concat([Buffer.concat([hashSN.slice(12), hashNN]), newNonce.slice(0, 4)]);
-        if (logger.isDebugEnabled()) logger.debug('aesIv = %s', aesIv.toString('hex'));
+        if (logger.isDebugEnabled()) {
+            logger.debug('aesKey = %s, aesIv = %s', aesKey.toString('hex'), aesIv.toString('hex'));
+        }
         // Decrypt the message
-        var answerWithHash = crypto.aesDecrypt({
-            msg: obj.serverDHParams.encrypted_answer,
-            key: aesKey,
-            iv: aesIv
-        });
+        var answerWithHash = crypto.aesDecrypt(
+            obj.serverDHParams.encrypted_answer,
+            aesKey,
+            aesIv
+        );
+        // Save AES key
+        obj.aes = {key: aesKey, iv: aesIv};
+        // De-serialize the inner data
         if (logger.isDebugEnabled()) logger.debug('answerWithHash(%s) = %s', answerWithHash.length, answerWithHash.toString('hex'));
         var answer = answerWithHash.slice(20, 564 + 20);
         if (logger.isDebugEnabled()) logger.debug('answer(%s) = %s', answer.length, answer.toString('hex'));
-        // De-serialize the inner data
         var serverDHInnerData = new mtproto.Server_DH_inner_data({
             buffer: answer
         }).deserialize();
@@ -227,6 +235,46 @@ TelegramLink.prototype.authorization = function (callback) {
         // Synch the local time with the server time
         crypto.timeSynchronization(serverDHInnerData.server_time * 1000, duration);
         obj.serverDHInnerData = serverDHInnerData;
+        return obj;
+    }
+
+    // Calculate the g_b and create the client DH parametersr
+    function createClientDHInnerData(obj) {
+        var retryCount = 0;
+        if (logger.isDebugEnabled()) logger.debug('Start calculating g_b');
+        // Calculate g_b
+        var g = obj.serverDHInnerData.g;
+        var b = crypto.createNonce(256);
+        var dhPrime = obj.serverDHInnerData.dh_prime;
+        var gb = crypto.modPow(g, b, dhPrime);
+        if (logger.isDebugEnabled()) logger.debug('g_b(%s) = %s', gb.length, gb.toString('hex'));
+        // Create client DH inner data
+        obj.clientDHInnerData = new mtproto.Client_DH_inner_data({props: {
+            nonce: obj.resPQ.nonce,
+            server_nonce: obj.resPQ.server_nonce,
+            retry_id: retryCount,
+            g_b: gb
+        }}).serialize();
+        return obj;
+    }
+
+    // Encrypt Client DH inner data
+    function encryptClientDHInnerDataWithAES(obj) {
+        var hash =  crypto.createSHA1Hash(obj.clientDHInnerData);
+        var dataWithHash = Buffer.concat([hash, obj.clientDHInnerData]);
+        if (logger.isDebugEnabled()) {
+            logger.debug('Data to be encrypted contains: hash(%s), clientDHInnerData(%s), total length %s',
+                hash.length, obj.clientDHInnerData.length, dataWithHash.length);
+        }
+        obj.encryptClientDHInnerData = crypto.aesEncrypt(
+            dataWithHash,
+            obj.aes.key,
+            obj.aes.iv
+        );
+        if (logger.isDebugEnabled()) {
+            logger.debug('encryptClientDHInnerData(%s) = %s',
+                obj.encryptClientDHInnerData.length, obj.encryptClientDHInnerData.toString('hex'));
+        }
         return obj;
     }
 };
