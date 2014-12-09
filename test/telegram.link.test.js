6874be617006150bd8a5a1d7f91d6bd1ff5b9103
Enrico Stara <enrico.stara@gmail.com>
2014-12-09 15:44:33 +0100

    Add `createAuthKey` method

--- test/telegram.link.test.js
+++ test/telegram.link.test.js
@@ -6,44 +6,61 @@ describe('TelegramLink', function () {
     var primaryDC = staticInfo.telegram.test.primaryDataCenter;
 
     describe('#connect()', function () {
-        it('should connect and disconnect to primary DC', function (done) {
+        it('should connect and disconnect from a primary DC with HTTP', function (done) {
             var telegramLink = new TelegramLink(primaryDC);
             telegramLink.connect(function (ex) {
-                if(ex) console.log(ex);
+                if (ex) console.log(ex);
                 else telegramLink.end(function (ex) {
-                    if(ex) console.log(ex);
+                    if (ex) console.log(ex);
                     done();
                 });
             });
-        })
+        });
+
+        it('should connect and disconnect from a primary DC with TCP', function (done) {
+            var telegramLink = new TelegramLink(primaryDC, 'TCP');
+            telegramLink.connect(function (ex) {
+                if (ex) console.log(ex);
+                else telegramLink.end(function (ex) {
+                    if (ex) console.log(ex);
+                    done();
+                });
+            });
+        });
     });
 
-    describe('#authorization()', function () {
-        it('should returns', function (done) {
 
+    describe('#getAuthorization()', function () {
+        var callback = function (ex, authKey) {
+            var self = arguments.callee;
+            if (ex) {
+                console.log('Authorization KO: %s', ex);
+                self.telegramLink.end();
+            }
+            else {
+                authKey.should.be.ok;
+                console.log('Authorization OK: %s', authKey.toString());
+                self.telegramLink.end();
+            }
+            (!ex).should.be.true;
+            self.done();
+        };
+
+        it('should returns AuthKey using a TCP connection', function (done) {
+            this.timeout(120000);
+            var telegramLink = new TelegramLink(primaryDC, 'TCP');
+            callback.telegramLink = telegramLink;
+            callback.done = done;
+            telegramLink.getAuthorization(callback);
+        });
+
+        it('should returns AuthKey using a HTTP connection', function (done) {
             this.timeout(120000);
             var telegramLink = new TelegramLink(primaryDC);
-            telegramLink.connect(function (e) {
-                if(e) {
-                    console.log('Connection error: %s', e);
-                    done();
-                    return;
-                }
-                telegramLink.authorization(function (ex, authKey) {
-                    if (ex) {
-                        console.log('Authorization KO: %s', ex);
-                        telegramLink.end();
-                    }
-                    else {
-                        authKey.should.be.ok;
-                        console.log('Authorization OK: %s', authKey.toString());
-                        telegramLink.end();
-                    }
-                    (!ex).should.be.true;
-                    done();
-                });
-            });
-        })
+            callback.telegramLink = telegramLink;
+            callback.done = done;
+            telegramLink.getAuthorization(callback);
+        });
     });
 });
 
