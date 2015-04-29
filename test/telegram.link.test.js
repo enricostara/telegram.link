da4fc9e2bc9c5d0cde95ef9a8e095880afdfca62
Enrico Stara <enrico.stara@gmail.com>
2015-04-29 08:26:59 +0200

    Add new features - review the 'createClient' method - create the encrypted channel - add the 'sendCodeToPhone' method - update unit test

--- test/telegram.link.test.js
+++ test/telegram.link.test.js
@@ -1,31 +1,46 @@
 require('should');
 require('requirish')._(module);
-var telegramLink = require('lib/telegram.link');
+var telegramLink = require('lib/telegram.link')();
+var net = require('telegram-mt-node').net;
+
 
 describe('TelegramLink', function () {
 
-    var appId = require('lib/static').telegram.test.appId;
     var primaryDC = telegramLink.TEST_PRIMARY_DC;
+
+    function getApp() {
+        return {
+            id: require('lib/static').telegram.test.app.id,
+            hash: require('lib/static').telegram.test.app.hash,
+            version: '1.0.0',
+            lang: 'en'
+        };
+    }
 
     describe('#createClient()', function () {
         it('should create a client', function () {
-            var client = telegramLink.createClient(appId, primaryDC);
-            client.should.be.ok;
-            client._appId.should.equal(appId);
-            (!client._authKey).should.be.true;
 
-            client = telegramLink.createClient(appId, primaryDC, {authKey: {id: 'id'}});
+            var app = getApp();
+            var client = telegramLink.createClient(app, primaryDC);
             client.should.be.ok;
-            client._appId.should.equal(appId);
-            client._authKey.should.be.ok;
-            client._authKey.id.should.be.equal('id');
+            client._app.id.should.equal(app.id);
+            client._app.should.have.properties(['id', 'hash', 'version', 'lang']);
+            (!client._channel).should.be.true;
+
+            app.authKey = {id: 'id'};
+
+            client = telegramLink.createClient(app, primaryDC);
+            client.should.be.ok;
+            client._app.should.have.properties(['id', 'hash', 'version', 'lang', 'authKey']);
+            client.isReady().should.be.true;
         });
     });
 
     describe('#createClient()', function () {
         it('should callback on the HTTP connection event', function (done) {
-            telegramLink.createClient(appId, primaryDC, function () {
-                console.log('connected');
+
+            var app = getApp();
+            telegramLink.createClient(app, primaryDC, function () {
                 done();
             });
         });
@@ -33,17 +48,25 @@ describe('TelegramLink', function () {
 
     describe('#createClient()', function () {
         it('should callback on the TCP connection event', function (done) {
-            var client = telegramLink.createClient(appId, primaryDC, {connectionType: 'TCP'}, function () {
-                client.end(done);
+
+            var app = getApp();
+            app.connectionType = 'TCP';
+            var client = telegramLink.createClient(app, primaryDC, function () {
+                client._connection.should.be.an.instanceOf(net.TcpConnection);
+                client.end(function() {
+                    done();
+                });
             });
             client.on(telegramLink.EVENT.ERROR, function (ex) {
-                console.log(ex);
+                console.error(ex);
                 done();
             })
         });
     });
 
+
     describe('#createAuthKey()', function () {
+        var app = getApp();
         var auth = require('telegram-mt-node').auth;
         // mock the 'createAuthKey' function
         auth.createAuthKey = function (callback, channel) {
@@ -60,18 +83,20 @@ describe('TelegramLink', function () {
             }, 0);
         };
         it('should returns AuthKey', function (done) {
-            var client = telegramLink.createClient(appId, primaryDC, function () {
+            var client = telegramLink.createClient(app, primaryDC, function () {
                 console.log('%s connected', client);
                 client.createAuthKey(function (auth) {
                     auth.key.should.be.ok;
                     auth.serverSalt.should.be.ok;
                     console.log('Auth key OK: %s', auth.key.toString());
+                    client._app.should.have.properties(['id', 'hash', 'version', 'lang']);
+                    client.isReady().should.be.true;
                     client.end(done);
                 });
             });
         });
         it('should returns an error', function (done) {
-            var client = telegramLink.createClient(appId, primaryDC, function () {
+            var client = telegramLink.createClient(app, primaryDC, function () {
                 var conn = client._connection;
                 client._connection = null;
                 client.once(telegramLink.EVENT.ERROR, function (ex) {
